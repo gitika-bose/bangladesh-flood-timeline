433ad61397f7bad5257f4482774be0fb167672f4
Igor Khomyakov <ikh@iri.columbia.edu>
2020-08-21 02:03:02 -0400

    Added articles to tagtog.py

--- tagtog/tagtog.py
+++ tagtog/tagtog.py
@@ -3,12 +3,14 @@ import os
 import sys
 from pathlib import Path
 import json
-#import yaml
+
+# import yaml
 import csv
 from typing import Dict, Any
 
 root_path = Path("/Users/ikh/Downloads/bangladesh_floods_20200820")
 ann_path = Path(root_path, "ann.json")
+article_path = Path(root_path, "plain.html")
 master_ann_path = Path(ann_path, "master")
 members_ann_path = Path(ann_path, "members")
 ann_legend_path = Path(root_path, "annotations-legend.json")
@@ -17,10 +19,19 @@ with open(ann_legend_path, "r") as f:
     legend = json.load(f)
 
 
-def traverse(output: Dict[str, Any], root_path: Path, path: Path) -> None:
+def empty_entry(legend: Dict[str, str]) -> Dict[str, Any]:
+    rs: Dict[str, Any] = {}
+    for k in legend.keys():
+        if k[0] == "m":
+            rs[legend[k]] = None
+    rs["__anncomplete"] = False
+    return rs
+
+
+def traverse_annotations(output: Dict[str, Any], root_path: Path, path: Path) -> None:
     for x in os.scandir(path):
         if x.is_dir():
-            traverse(output, root_path, Path(x.path))
+            traverse_annotations(output, root_path, Path(x.path))
         else:
             folder = str(Path(x.path).parent)[len(str(root_path)) :]
             article_id = Path(x.path).with_suffix("").stem
@@ -28,27 +39,51 @@ def traverse(output: Dict[str, Any], root_path: Path, path: Path) -> None:
                 annotations = json.load(f)
             anncomplete = annotations["anncomplete"]
             metas = annotations["metas"]
-            metas_converted = {legend[k]: v["value"] for k, v in metas.items()}
-            for k in legend.keys():
-                if k[0] == "m" and k not in metas:
-                    metas_converted[legend[k]] = None
-            output.append(
-                dict(
-                    __article_id=article_id,
-                    __folder=folder,
-                    __anncomplete=anncomplete,
-                    **metas_converted
-                )
+            output[article_id] = dict(
+                __article_id=article_id,
+                __folder=folder,
+                __anncomplete=anncomplete,
+                **{legend[k]: v["value"] for k, v in metas.items()}
             )
 
 
-rs = []
-traverse(rs, master_ann_path, master_ann_path)
+def traverse_articles(output: Dict[str, Any], root_path: Path, path: Path) -> None:
+    for x in os.scandir(path):
+        if x.is_dir():
+            traverse_articles(output, root_path, Path(x.path))
+        else:
+            folder = str(Path(x.path).parent)[len(str(root_path)) :]
+            article_id = Path(x.path).with_suffix("").stem
+            with open(x.path, "r") as f:
+                article = f.read()
+            length = len(article)
+            output[article_id] = dict(
+                __article_id=article_id,
+                __folder=folder,
+                __content=article,
+                __length=length,
+            )
 
-if len(rs) != 0:
-    w = csv.DictWriter(sys.stdout, rs[0].keys())
+
+articles: Dict[str, Any] = {}
+traverse_articles(articles, article_path, article_path)
+
+annotations: Dict[str, Any] = {}
+traverse_annotations(annotations, master_ann_path, master_ann_path)
+
+if len(articles) != 0:
+    keys = list(
+        set(empty_entry(legend).keys())
+        | set(annotations[next(iter(annotations))].keys())
+        | set(articles[next(iter(articles))].keys())
+    )
+    w = csv.DictWriter(sys.stdout, keys)
     w.writeheader()
-    for r in rs:
+    for k in sorted(articles.keys()):
+        r = empty_entry(legend)
+        if k in annotations:
+            r.update(annotations[k])
+        r.update(articles[k])
         w.writerow(r)
 
 # yaml.dump(rs, sys.stdout, default_flow_style=False, width=120, allow_unicode=True)
